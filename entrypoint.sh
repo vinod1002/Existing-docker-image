1b902f9905fa6bb3f2e7334af16958fec956aa3c
vinod1002 <158429600+vinod1002@users.noreply.github.com>
2024-06-03 11:45:22 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,15 @@
 
 set -e
 
-# Install Python dependencies using apk
-apk update
-apk add python3 py3-pip # Install Python 3 and pip for Python 3
+# Install Python and pip
+apk add --no-cache python3 py3-pip
 
 # Install Python dependencies using pip
 if [ -n "$1" ]; then
   echo "Installing Python dependencies: $1"
-  pip3 install "$1"
-  echo "::set-output name=python-dependencies-installed::true"
+  pip3 install $1
+  echo "::set-output name=python_dependencies_installed::true"
 else
   echo "No Python dependencies specified."
-  echo "::set-output name=python-dependencies-installed::false"
+  echo "::set-output name=python_dependencies_installed::false"
 fi
