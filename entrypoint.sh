e5e88c85de8b68bfd6227f8ff0e6dc624e5de666
vinod1002 <158429600+vinod1002@users.noreply.github.com>
2024-06-03 10:44:18 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,16 @@
 
 set -e
 
+# Update package lists
+apt-get update
+
 # Install Python dependencies using apt
-apt-get update && apt-get install -y python3 py3-pip # Install Python 3 and pip for Python 3
+apt-get install -y python3 py3-pip # Install Python 3 and pip for Python 3
 
 # Install Python dependencies using pip
 if [ -n "$1" ]; then
   echo "Installing Python dependencies: $1"
-  pip3 install $1
+  pip3 install "$1"  # Wrap package names in double quotes
   echo "::set-output name=python-dependencies-installed::true"
 else
   echo "No Python dependencies specified."
