931297413462a9e1f3a2a7609dc0a3fa18ee14d8
vinod1002 <158429600+vinod1002@users.noreply.github.com>
2024-06-03 16:37:18 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
-
 set -e
 
-# Copy entrypoint.sh from the GitHub repository to /python/entrypoint.sh inside the container
-cp entrypoint.sh /entrypoint.sh
-
-# Make entrypoint.sh executable
-chmod +x /entrypoint.sh
-
-# Install Python dependencies using apt
-apt-get update && apt-get install -y python3 py3-pip # Install Python 3 and pip for Python 3
-
-# Install Python dependencies using pip
+# Install the Python dependencies
 if [ -n "$1" ]; then
-  echo "Installing Python dependencies: $1"
-  pip3 install "$1"
-  echo "::set-output name=python_dependencies_installed::true"
+    pip3 install $1
+    echo "python_dependencies_installed=true" >> $GITHUB_ENV
 else
-  echo "No Python dependencies specified."
-  echo "::set-output name=python_dependencies_installed::false"
+    echo "No python dependencies specified"
+    echo "python_dependencies_installed=false" >> $GITHUB_ENV
 fi
