69c316324b4db9d2ad612e0d6bc766669a27a484
vinod1002 <158429600+vinod1002@users.noreply.github.com>
2024-06-04 10:48:04 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
-set -e
 
-# Install the Python dependencies
-if [ -n "$1" ]; then
-    pip3 install $1
-    echo "python_dependencies_installed=true" >> $GITHUB_ENV
-else
-    echo "No python dependencies specified"
-    echo "python_dependencies_installed=false" >> $GITHUB_ENV
-fi
+# Variables
+IMAGE_NAME="docker:latest"
+CONTAINER_NAME="my-container"
+SCRIPT_CONTENT="#!/bin/sh\n echo 'Hello, Docker!'"
+SCRIPT_NAME="entrypoint.sh"
+DESTINATION_PATH="/usr/src/app/$SCRIPT_NAME"
+
+# Create the script
+echo -e $SCRIPT_CONTENT > $SCRIPT_NAME
+chmod +x $SCRIPT_NAME
+
+# Pull the Docker image
+docker pull $IMAGE_NAME
+
+# Run the container with Docker socket mounted for Docker-in-Docker (DinD)
+docker run -d --name $CONTAINER_NAME -v /var/run/docker.sock:/var/run/docker.sock $IMAGE_NAME tail -f /dev/null
+
+# Copy the script into the container
+docker cp $SCRIPT_NAME $CONTAINER_NAME:$DESTINATION_PATH
+
+# Make the script executable inside the container
+docker exec $CONTAINER_NAME chmod +x $DESTINATION_PATH
+
+# Execute the script inside the container
+docker exec $CONTAINER_NAME $DESTINATION_PATH
